08514c0a42270b7ab54b4745e53802026d91ac98
lmxing <medgov@163.com>
2020-09-01 21:07:52 -0500

    Update my_list_node.h

--- my_list_node.h
+++ my_list_node.h
@@ -4,19 +4,19 @@
 
 struct ListNode
 {
-	int val;
-	ListNode *next;
-	ListNode (int x): val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode (int x): val(x), next(NULL) {}
 };
 
 ListNode * buildListNode(vector<int> & nums) {
-	ListNode * head = new ListNode(-1);
-	ListNode * cur = head;
-	for (auto num: nums) {
-		ListNode *tmp = new ListNode(num);
-		cur->next = tmp;
-		cur = tmp;
-	}
-	return head->next;
+    ListNode * head = new ListNode(-1);
+    ListNode * cur = head;
+    for (auto num: nums) {
+        ListNode *tmp = new ListNode(num);
+	cur->next = tmp;
+	cur = tmp;
+    }
+    return head->next;
 }
 
